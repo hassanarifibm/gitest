46ba90e2dc97ab96e2b9a5dda84e164b4e0b1da3
hassanarifibm <hassanarifibm@gmail.com>
2023-01-29 12:39:27 -0500

    test-from-main

--- gitesting/src/gitesting/MainClass.java
+++ gitesting/src/gitesting/MainClass.java
@@ -3,6 +3,6 @@ package gitesting;
 public class MainClass {
 	public static void main(String[] args0) {
 		System.out.println("Hassan Arif");
-		//testing for main program
+		//test-from-main
 	}
 }
