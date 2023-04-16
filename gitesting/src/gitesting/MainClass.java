6e77730f9bc38443b759bc71dab2e8e1893bf993
hassanarifibm <117517864+hassanarifibm@users.noreply.github.com>
2023-04-17 02:28:01 +0500

    Update MainClass.java

--- gitesting/src/gitesting/MainClass.java
+++ gitesting/src/gitesting/MainClass.java
@@ -3,6 +3,6 @@ package gitesting.src.gitesting;
 public class MainClass {
 	public static void main(String[] args0) {
 		System.out.println("Hassan Arif");
-		//testing for main program 
+		//testing for main program  
 	}
 }
